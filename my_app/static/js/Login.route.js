935a82dac2871d401f284a80c5fe706119314955
srpbarri <srpbarri@localhost.localdomain>
2023-06-03 01:51:58 -0600

    Prueba login

--- my_app/static/js/Login.route.js
+++ my_app/static/js/Login.route.js
@@ -1,19 +1,3 @@
 
-const { Router } = require('express')
-const loginController = require('static/js/login.controller')
-const router = Router()
-
-const permisos = async(req,res,next) => {
-    if(req.session.loggedin != true){
-        next()
-    }else{
-        res.render('home/index', {logueado: req.session.loggedin, admin: req.session.admin})
-    }
-}
-
-
- router.get('/login', permisos, loginController.index)
- router . post( '/login' , loginController.auth)
- router.get('/logout', loginController.logout)
-
- module.exports = router
+const username = document.getElementById('username')
+const password = document.getElementById('password')
